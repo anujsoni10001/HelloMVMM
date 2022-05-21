bd01f9a1a749b754b591e594fd537cde71099f35
Anuj Soni <48941274+anujsoni10001@users.noreply.github.com>
2022-05-21 06:54:09 +0530

    Initial Commit

---
+++ HelloMVMM/HelloMVMMApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloMVMMApp.swift
+//  HelloMVMM
+//
+//  Created by Anuj Soni on 21/05/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloMVMMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
